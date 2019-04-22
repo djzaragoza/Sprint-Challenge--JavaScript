802193942d6d1c9f9132c1ad3312f833a9346e23
DJ Zaragoza <zaragoza0126@gmail.com>
2019-04-22 11:04:39 -0700

    fixed the syntax error in classes.js file per PM request.  Everything is good now

--- challenges/classes.js
+++ challenges/classes.js
@@ -7,41 +7,43 @@ function CuboidMaker(length, width, height) {
 }
 
 
-// CuboidMaker.prototype.volume = function () {
-//     return this.length * this.width * this.height
-// }
+CuboidMaker.prototype.volume = function () {
+    return this.length * this.width * this.height
+}
 class volume {
     constructor(length, width, height) {
         this.length = length;
         this.width = width;
         this.height = height;
     }
+}
 
+CuboidMaker.prototype.surfaceArea = function () {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+}
 
-// CuboidMaker.prototype.surfaceArea = function () {
-//     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
-// }
 class surfaceArea {
     constructor(length, width, height) {
         this.length = length;
         this.width = width;
         this.height = height;
     }   
-}
-
-get surfaceArea() {
-    return this.calcArea();
-}
+    get surfaceArea() {
+        return this.calcArea()
+    }
     calcArea() {
-        return this.length * this.width * this.height;
+        return this.length * this.width * this.height
+    }
 }
-
-
+     
 const square = new surfaceArea(4, 5, 5);
+
+cuboid = new CuboidMaker(4, 5, 5)
 
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
+
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
