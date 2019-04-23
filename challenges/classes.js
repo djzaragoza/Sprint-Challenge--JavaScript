c82ebdd233e24f6278d7f829819af1ae9c1b9d82
DJ Zaragoza <zaragoza0126@gmail.com>
2019-04-22 18:47:00 -0700

    modified classes.js file for Sprint with PM.

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,15 +1,15 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-function CuboidMaker(length, width, height) {
-    this.length = length
-    this.width = width
-    this.height = height
-}
+// function CuboidMaker(length, width, height) {
+//     this.length = length
+//     this.width = width
+//     this.height = height
+// }
 
 
-CuboidMaker.prototype.volume = function () {
-    return this.length * this.width * this.height
-}
+// CuboidMaker.prototype.volume = function () {
+//     return this.length * this.width * this.height
+// }
 class volume {
     constructor(length, width, height) {
         this.length = length;
@@ -18,9 +18,9 @@ class volume {
     }
 }
 
-CuboidMaker.prototype.surfaceArea = function () {
-    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
-}
+// CuboidMaker.prototype.surfaceArea = function () {
+//     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+// }
 
 class surfaceArea {
     constructor(length, width, height) {
@@ -28,22 +28,26 @@ class surfaceArea {
         this.width = width;
         this.height = height;
     }   
-    get surfaceArea() {
-        return this.calcArea()
+
+    calcSurfaceArea() {
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
     }
-    calcArea() {
+    calcVolume() {
         return this.length * this.width * this.height
     }
-}
-     
-const square = new surfaceArea(4, 5, 5);
 
-cuboid = new CuboidMaker(4, 5, 5)
+}
+
+
+     
+const square = new volume(4, 5, 5);
+
+const cuboid = new surfaceArea(4, 5, 5)
 
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.calcVolume()); // 100
+console.log(cuboid.calcSurfaceArea()); // 130
 
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
